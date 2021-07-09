875bc286913fd6511dfff6748050b7af28812dfa
JesusIasenzaniro <16jesusvicente@gmail.com>
2021-07-09 21:46:15 +0200

    setting basic structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Search from './pages/Search';
+import MyList from './pages/MyList';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <Router>
+                <Header />
+                <Switch>
+                    <Route exact path='/' component={Search} />
+                    <Route exact path='/Mylist' component={MyList} />
+                </Switch>
+                <Footer />
+            </Router>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/pages/MyList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Mylist = () => {
+    return <div>MyList</div>;
+};
+
+export default Mylist;
